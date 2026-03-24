1785777c01160493a42e253495e65b9d6fe38253
kzztein <santos.kylerafael@ue.edu.ph>
2026-03-24 11:01:56 +0800

    Update navbar to conditionally apply hero class

--- includes/navbar.php
+++ includes/navbar.php
@@ -2,11 +2,12 @@
 $current_page = basename($_SERVER['PHP_SELF']);
 $logged_in = isset($_SESSION['user_id']);
 $user_name = $logged_in ? $_SESSION['user_name'] : '';
+$has_hero = ($current_page === 'index.php');
 ?>
-<nav class="navbar navbar-expand-lg fixed-top py-2" id="mainNav">
+<nav class="navbar navbar-expand-lg fixed-top py-2 <?= $has_hero ? 'navbar-hero' : 'navbar-solid' ?>" id="mainNav">
   <div class="container">
     <a class="navbar-brand d-flex align-items-center gap-2" href="index.php">
-      <img src="logo.png" alt="The Lighthouse Logo" style="height:52px; width:auto; object-fit:contain;">
+      <img src="logo.png" alt="The Lighthouse Logo" style="height:52px;width:auto;object-fit:contain;background:transparent;">
       <span class="brand-text">THE LIGHTHOUSE</span>
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav">
@@ -29,7 +30,7 @@ $user_name = $logged_in ? $_SESSION['user_name'] : '';
       </ul>
       <div class="d-flex ms-lg-3 gap-2">
         <?php if ($logged_in): ?>
-          <span class="navbar-text fw-medium me-2" style="color:white;">Hi, <?= htmlspecialchars($user_name) ?>!</span>
+          <span class="navbar-text fw-medium me-2">Hi, <?= htmlspecialchars($user_name) ?>!</span>
           <a href="logout.php" class="btn btn-outline-light rounded-pill px-4">Logout</a>
         <?php else: ?>
           <a href="login.php" class="btn btn-outline-light rounded-pill px-4">Sign In</a>
